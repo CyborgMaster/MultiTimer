9e85d8790d2d6d92b51f33c94eb009cc689af201
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2013-04-24 14:46:12 -0600

    Styling of timers

--- sandbox.js
+++ sandbox.js
@@ -2,16 +2,7 @@ var templates = {};
 var env = new nunjucks.Environment({});
 
 env.addFilter('formatTimer', function(millis) {
-  var sec = Math.ceil(millis / 1000);
-  var hours = Math.floor(sec / 3600);
-  var minutes = Math.floor((sec - (hours * 3600)) / 60);
-  var seconds = sec - (hours * 3600) - (minutes * 60);
-
-  if (seconds < 10) { seconds = "0"+seconds; }
-  if (hours > 0 && minutes < 10) { minutes = "0"+minutes; }
-  var time = minutes+':'+seconds;
-  if (hours > 0) { time = hours+':'+time; }
-  return time;
+  return formatTimer(millis);
 });
 
 env.addFilter('remaining', function(start, length) {
--- window.js
+++ window.js
@@ -11,19 +11,26 @@ var updateTimers = function() {
   var $timers = $('.timer'), i;
   //check for new timers
   if ($timers.length < timers.length) {
-    for (i = 0; i < timers.length - $timers.length; i++) {
-      nunjucks.render('timer', {timer: timers[0]}, function(html) {
-        $(html.trim()).appendTo('#timers');
-      });
+    for (i = $timers.length; i < timers.length; i++) {
+      nunjucks.render('timer', {}, (function(i) { return function(html) {
+        var $timer = $(html.trim());
+        updateTimer($timer, timers[i]);
+        $timer.appendTo('#timers');
+      };})(i));
     }
   } //TODO add delete
 
   //update timers
   $('.timer').each(function(index) {
-    var $timer = $(this), timer = timers[index];
-    $timer.find('.remaining').text(timer.remaining);
+    updateTimer($(this), timers[index]);
   });
 
+};
+
+var updateTimer = function($timer, timer) {
+  $timer.find('.title').text(timer.title);
+  $timer.find('.duration').text(formatTimer(timer.length));
+  $timer.find('.remaining').text(formatTimer(timer.remaining));
 };
 
 
